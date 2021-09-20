47ae050de85768e4d7eafb254d11d843ae709041
Daniel Sanchez <dan.sanchez823@gmail.com>
2021-09-20 10:00:30 -0700

    style(nav): format using prettier

--- application/src/components/nav/nav.js
+++ application/src/components/nav/nav.js
@@ -4,35 +4,34 @@ import { Link, useHistory } from "react-router-dom";
 import "./nav.css";
 import { logout } from "../login/loginSlice";
 
-
 const Nav = () => {
-    const dispatch = useDispatch();
-    const history = useHistory();
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-    const onLogout = () => {
-        dispatch(logout());
-        history.push('/login');
-    }
+  const onLogout = () => {
+    dispatch(logout());
+    history.push("/login");
+  };
 
-    return (
-        <div className="nav-strip">
-            <Link to={"/order"} className="nav-link">
-                <div className="nav-link-style">
-                    <label className="nav-label">Order Form</label>
-                </div>
-            </Link> 
-            <Link to={"/view-orders"} className="nav-link" id="middle-link">
-                <div className="nav-link-style">
-                    <label className="nav-label">View Orders</label>
-                </div>
-            </Link>
-            <div onClick={onLogout} className="nav-link">
-                <div className="nav-link-style">
-                    <label className="nav-label">Log Out</label>
-                </div>
-            </div>
+  return (
+    <div className="nav-strip">
+      <Link to={"/order"} className="nav-link">
+        <div className="nav-link-style">
+          <label className="nav-label">Order Form</label>
         </div>
-    );
-}
+      </Link>
+      <Link to={"/view-orders"} className="nav-link" id="middle-link">
+        <div className="nav-link-style">
+          <label className="nav-label">View Orders</label>
+        </div>
+      </Link>
+      <div onClick={onLogout} className="nav-link">
+        <div className="nav-link-style">
+          <label className="nav-label">Log Out</label>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Nav;
