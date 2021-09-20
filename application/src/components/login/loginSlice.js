f7b90b126a5bf3c0c5fd7e98c960fcc5b51f0f38
Daniel Sanchez <dan.sanchez823@gmail.com>
2021-09-20 09:59:50 -0700

    style(login): format using prettier

--- application/src/components/login/loginSlice.js
+++ application/src/components/login/loginSlice.js
@@ -2,55 +2,51 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { SERVER_IP } from "../../private";
 
 const INITIAL_STATE = {
-    email: "",
-    token: "",
-} 
-
+  email: "",
+  token: "",
+};
 
 const performLogin = async (email, password) => {
-   
-   try {
-       const loginResponse = await fetch(`${SERVER_IP}/api/login`, {
-        method: 'POST',
-        body: JSON.stringify({
-            email,
-            password
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    }).then(response => response.json())
-    if(loginResponse.success) {
-        return {
-            email: loginResponse.email,
-            token: loginResponse.token
-        }
+  try {
+    const loginResponse = await fetch(`${SERVER_IP}/api/login`, {
+      method: "POST",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((response) => response.json());
+    if (loginResponse.success) {
+      return {
+        email: loginResponse.email,
+        token: loginResponse.token,
+      };
     } else {
-        return {...INITIAL_STATE}
+      return { ...INITIAL_STATE };
     }
-   } catch (error) {
-       console.error(error);
-       return {...INITIAL_STATE}
-   }
-    
-}
+  } catch (error) {
+    console.error(error);
+    return { ...INITIAL_STATE };
+  }
+};
 
-export const login = createAsyncThunk('login/loginStatus', performLogin);
+export const login = createAsyncThunk("login/loginStatus", performLogin);
 
 const loginSlice = createSlice({
-    name: "login",
-    initialState: INITIAL_STATE,
-    reducers: {
-        logout: (state, action) => {
-            state = {...INITIAL_STATE}
-        }
+  name: "login",
+  initialState: INITIAL_STATE,
+  reducers: {
+    logout: (state, action) => {
+      state = { ...INITIAL_STATE };
     },
-    extraReducers: builder => {
-        builder.addCase(login.fulfilled, (state, action) => {
-            state = {...action.payload}
-        })
-    }
-
+  },
+  extraReducers: (builder) => {
+    builder.addCase(login.fulfilled, (state, action) => {
+      state = { ...action.payload };
+    });
+  },
 });
 
 export default loginSlice.reducer;
