43d25f67134767ffd1fd964f95e5524ba76e375d
Daniel Sanchez <dan.sanchez823@gmail.com>
2021-09-24 23:20:46 -0700

    fix(nav): fix logout #7

--- application/src/components/nav/nav.js
+++ application/src/components/nav/nav.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { logoutUser } from "../../redux/actions/authActions";
 import "./nav.css";
 
 const Nav = (props) => {
+
+  const dispatch = useDispatch();
+
   return (
     <div className="nav-strip">
       <Link to={"/order"} className="nav-link">
@@ -15,11 +20,11 @@ const Nav = (props) => {
           <label className="nav-label">View Orders</label>
         </div>
       </Link>
-      <Link to={"/login"} className="nav-link">
+      <button onClick={() => dispatch(logoutUser())} className="nav-link">
         <div className="nav-link-style">
           <label className="nav-label">Log Out</label>
         </div>
-      </Link>
+      </button>
     </div>
   );
 };
