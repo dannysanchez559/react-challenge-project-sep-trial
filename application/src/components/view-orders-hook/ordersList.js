5feec194d0e2eaf9aa859c8ae971a8f4f1441250
Daniel Sanchez <dan.sanchez823@gmail.com>
2021-09-27 19:43:41 -0700

    feat(orderslist): implement delete order feature----has not been tested due to cors error

--- application/src/components/view-orders-hook/ordersList.js
+++ application/src/components/view-orders-hook/ordersList.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { SERVER_IP } from "../../private";
+
+const DELETE_ORDER_URL = `${SERVER_IP}/api/delete-order`;
 
 const OrdersList = (props) => {
     const { orders } = props;
+    const [currentOrder, setCurrentOrder] = useState(0);
+
     if (!props || !props.orders || !props.orders.length) return (
         <div className="empty-orders">
             <h2>There are no orders to display</h2>
         </div>
     );
+
+    const deleteButtonClicked = (order) => {
+        console.log("order # ", order);
+
+        // remove current order item
+        fetch(DELETE_ORDER_URL, {
+            method: 'DELETE',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({'id': order})  //if you do not want to send any addional data,  replace the complete JSON.stringify(YOUR_ADDITIONAL_DATA) with null
+          })
+          .then((res) => res.json())
+          .then((response) => console.log("Success", JSON.stringify(response)))
+          .catch((error) => console.error(error));
+          
+    }
 
     return orders.map(order => {
         const createdDate = new Date(order.createdAt);
@@ -22,7 +44,7 @@ const OrdersList = (props) => {
                 </div>
                 <div className="col-md-4 view-order-right-col">
                     <button className="btn btn-success">Edit</button>
-                    <button className="btn btn-danger">Delete</button>
+                    <button className="btn btn-danger" onClick={() => deleteButtonClicked(order._id)}>Delete</button>
                 </div>
             </div>
         );
