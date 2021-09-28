58412a2188224b266afe610bf36c759ebcba56de
Daniel Sanchez <dan.sanchez823@gmail.com>
2021-09-27 20:14:51 -0700

    feat(nav): display user email in nav area

--- application/src/components/nav/nav.js
+++ application/src/components/nav/nav.js
@@ -1,26 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import "./nav.css";
 
 const Nav = (props) => {
+
+  const userEmail = useSelector((state) => state.auth.email);
+
   return (
-    <div className="nav-strip">
-      <Link to={"/order"} className="nav-link">
-        <div className="nav-link-style">
-          <label className="nav-label">Order Form</label>
-        </div>
-      </Link>
-      <Link to={"/view-orders"} className="nav-link" id="middle-link">
-        <div className="nav-link-style">
-          <label className="nav-label">View Orders</label>
-        </div>
-      </Link>
-      <Link to={"/login"} className="nav-link">
-        <div className="nav-link-style">
-          <label className="nav-label">Log Out</label>
-        </div>
-      </Link>
-    </div>
+    <>
+      <div className="userEmail">
+        <p>Welcome {userEmail}!</p>
+      </div>
+      <div className="nav-strip">
+        <Link to={"/order"} className="nav-link">
+          <div className="nav-link-style">
+            <label className="nav-label">Order Form</label>
+          </div>
+        </Link>
+        <Link to={"/view-orders"} className="nav-link" id="middle-link">
+          <div className="nav-link-style">
+            <label className="nav-label">View Orders</label>
+          </div>
+        </Link>
+        <Link to={"/login"} className="nav-link">
+          <div className="nav-link-style">
+            <label className="nav-label">Log Out</label>
+          </div>
+        </Link>
+      </div>
+    </>
   );
 };
 
